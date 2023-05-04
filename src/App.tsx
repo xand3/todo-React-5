886bac20e6569bd9b51cf843cf3c52617034b600
Alexandre Bastos <xande.bastos321@gmail.com>
2023-05-03 23:26:08 -0300

    Feita a lógica para riscar tarefa concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import * as C from "./App.styles" 
+import { useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./Types/Item";
+import { ListItem } from "./Components/ListItem";
 
 const App = () => {
-  return(
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      task: "Desenvolver um projeto pessoal em React",
+      done: false,
+    },
+    {
+      id: 2,
+      task: "Conseguir um emprego como Desenvolvedor",
+      done: true,
+    },
+    {
+      id: 3,
+      task: "Aperfeiçõar meu inglês",
+      done: false,
+    },
+  ]);
+
+  return (
     <C.Container>
       <C.Area>
-        <C.Header>Lista de Tarefas</C.Header>
+        <>
+          <C.Header>Lista de Tarefas</C.Header>
+
+          {list.map((item, index) => (
+            <ListItem key={index} item={item}/>
+          ))}
+        </>
       </C.Area>
     </C.Container>
   );
-}
+};
 
 export default App;
---
+++ src/Types/Item.ts
@@ -0,0 +1,5 @@
+export type Item = {
+  id: number;
+  task: string;
+  done: boolean;
+};
